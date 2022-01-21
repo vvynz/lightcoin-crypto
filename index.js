669e3f8eabbcfcd32352d739a55450762542a6a6
Vivian Zhang <vvynz12@gmail.com>
2022-01-21 22:45:11 +0000

    refactored the code so that the withdrawal and deposit classes also use the new account object instead of the balance variable. Every time a withdrawal or deposit is created, it'll pass it to the account that is associated with the transaction

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 let balance = 500.00;
 
 class Withdrawal {
-
-  constructor(amount) {
+// pass in the account that the withdrawal is for
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
+// update the balance in the account
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
 
 class Deposit {
-  constructor(amount) {
+  // pass in the account that the deposit this is for
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
+// update the balance in the account
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 }
 
-
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+// t1 = new Withdrawal(50.25);
+// t1.commit();
+// console.log('Transaction 1:', t1);
 
-console.log('Balance:', balance);
+// t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00);
-t3.commit();
-console.log("Transaction 3:", t3);
+// console.log('Balance:', balance);
+
+// t3 = new Deposit(120.00);
+// t3.commit();
+// console.log("Transaction 3:", t3);
